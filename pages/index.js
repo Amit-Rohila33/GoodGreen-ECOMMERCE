fcb2fe500e7b682453d6b7172b5b015a6b541cb9
Amit Kumar <103894389+Amit-Rohila33@users.noreply.github.com>
2022-12-14 16:44:00 +0530

    Completed the pages development and the app set up

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>GoodGreen.com - The Green for life</title>
+        <meta name="description" content="GoodGreen.com - The Green for life" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div >
+        Home
+      </div>
+    </div>
+  );
+}
