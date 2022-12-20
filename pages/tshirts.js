66361f6421d012c417a213b8d95eee946651559f
Amit Kumar <103894389+Amit-Rohila33@users.noreply.github.com>
2022-12-20 13:58:07 +0530

    tshirts page color and size variant done

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -9,20 +9,20 @@ const Tshirts = ({ products }) => {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap justify-center -m-4">
-            {products.map((item) => {
+            {Object.keys(products).map((item) => {
               return (
                 <Link
-                  key={item._id}
+                  key={products[item]._id}
                   legacyBehavior
                   passHref={true}
-                  href={`/product/${item.slug}`}
+                  href={`/product/${products[item].slug}`}
                 >
                   <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-xl m-5 cursor-pointer">
                     <div className="block relative rounded overflow-hidden">
                       <img
                         alt="ecommerce"
                         className="h-[30vh] md:h-[36vh] m-auto block"
-                        src={item.img}
+                        src={products[item].img}
                       />
                     </div>
                     <div className="mt-4 text-center md:text-left">
@@ -30,10 +30,23 @@ const Tshirts = ({ products }) => {
                         T-Shirts
                       </h3>
                       <h2 className="text-gray-900 title-font text-lg font-medium">
-                        {item.title}
+                        {products[item].title}
                       </h2>
-                      <p className="mt-1">{item.price}</p>
-                      <p className="mt-1">S,M,L,XL,XXL</p>
+                      <p className="mt-1">{products[item].price}</p>
+                      <div className="mt-1">
+                        {products[item].size.includes('S') && <span className="border border-gray-600 mx-1 px-1">S</span>}
+                        {products[item].size.includes('M') && <span className="border border-gray-600 mx-1 px-1">M</span>}
+                        {products[item].size.includes('L') && <span className="border border-gray-600 mx-1 px-1">L</span>}
+                        {products[item].size.includes('XL') && <span className="border border-gray-600 mx-1 px-1">XL</span>}
+                        {products[item].size.includes('XXL') && <span className="border border-gray-600 mx-1 px-1">XXL</span>}
+                      </div>
+                      <div className="mt-1">
+                      {products[item].color.includes('red') && <button className="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes('yellow') && <button className="border-2 border-gray-300 ml-1 bg-yellow-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes('purple') && <button className="border-2 border-gray-300 ml-1 bg-purple-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes('green') && <button className="border-2 border-gray-300 ml-1 bg-green-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      {products[item].color.includes('black') && <button className="border-2 border-gray-300 ml-1 bg-black-700 rounded-full w-6 h-6 focus:outline-none"></button>}
+                      </div>
                     </div>
                   </div>
                 </Link>
@@ -52,9 +65,32 @@ export async function getServerSideProps(context) {
   }
 
   let products = await Product.find({ category: "tshirts" });
+  let tshirts = {};
+  for (let item of products) {
+    if (item.title in tshirts) {
+      if (
+        !tshirts[item.title].color.includes(item.color) &&
+        item.avaiableQty > 0
+      ) {
+        tshirts[item.title].color.push(item.color);
+      }
+      if (
+        !tshirts[item.title].size.includes(item.size) &&
+        item.avaiableQty > 0
+      ) {
+        tshirts[item.title].size.push(item.size);
+      }
+    } else {
+      tshirts[item.title] = JSON.parse(JSON.stringify(item));
+      if (item.avaiableQty > 0) {
+        tshirts[item.title].color = [item.color];
+        tshirts[item.title].size = [item.size];
+      }
+    }
+  }
 
   return {
-    props: { products: JSON.parse(JSON.stringify(products)) },
+    props: { products: JSON.parse(JSON.stringify(tshirts)) },
   };
 }
 
