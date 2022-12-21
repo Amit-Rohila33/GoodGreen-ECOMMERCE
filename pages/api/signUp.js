400a2d388ca2fbbc49afc41148c29a182620c57e
Amit Kumar <103894389+Amit-Rohila33@users.noreply.github.com>
2022-12-21 16:14:29 +0530

    cryptoJs authentication added to user

--- pages/api/signUp.js
+++ pages/api/signUp.js
@@ -1,11 +1,17 @@
 import User from "../../models/User";
 import connectDb from "../../middleware/mongoose";
+var CryptoJS = require("crypto-js");
 
 const handler = async (req, res) => {
   if (req.method == "POST") {
     // console.log(req.body)
-    let u = new User (req.body)
-    await u.save()
+    const { name, email } = req.body;
+    let u = new User({
+      name,
+      email,
+      password: CryptoJS.AES.encrypt(req.body.password, 'Messi123').toString(),
+    });
+    await u.save();
     res.status(200).json({ success: "success" });
   } else {
     res.status(400).json({ error: "This method is not allowed" });
