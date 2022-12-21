fc91e316d412ef71a0ec011e8d01911ad94ba0ed
Amit Kumar <103894389+Amit-Rohila33@users.noreply.github.com>
2022-12-21 16:35:14 +0530

    jwt token added

--- pages/api/signUp.js
+++ pages/api/signUp.js
@@ -9,7 +9,7 @@ const handler = async (req, res) => {
     let u = new User({
       name,
       email,
-      password: CryptoJS.AES.encrypt(req.body.password, 'Messi123').toString(),
+      password: CryptoJS.AES.encrypt(req.body.password, "Messi123").toString(),
     });
     await u.save();
     res.status(200).json({ success: "success" });
